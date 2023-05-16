7ef45c1277df6f11687619b20959527b6fbbce3c
Adhil Bin Ashraf <adhilbintv10@gmail.com>
2023-05-16 09:23:07 +0530

    test commit

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ app.use(bodyParser.json({limit:"30mb",extended: true}))
 app.use(bodyParser.urlencoded({limit:"30mb",extended: true}))
 app.use(cors())
 app.use('/asset',express.static(path.join(__dirname,'public/assets')))
+app.use('/asset',express.static(path.join(__dirname,'public/assets')))
 
 // file storage
 
